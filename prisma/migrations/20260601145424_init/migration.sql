d836f11dcc569448b47ee6c414f2a7340d3dcb88
tejayy <tbj456@gmail.com>
2026-06-01 20:28:59 +0530

    Removed .env from repo

--- prisma/migrations/20260601145424_init/migration.sql
+++ prisma/migrations/20260601145424_init/migration.sql
@@ -1,9 +1,14 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "refreshToken" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updateAt" TIMESTAMP(3) NOT NULL,
 
